ed7494e8a79c5b892ed6c53047a84daef3412b80
Gaston De Sade <gastondesade@proton.me>
2025-11-21 04:03:18 +0100

    Refactor OrdersController to use constructor injection and add GetOrdersByUserId method

--- N_Tier.API/Controllers/OrderController.cs
+++ N_Tier.API/Controllers/OrderController.cs
@@ -1,42 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using N_Tier.BLL.DTOs;
-using N_Tier.BLL.Entities;
 using N_Tier.BLL.Manager.Interfaces.Services;
 
 namespace N_Tier.API.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class OrdersController : ControllerBase
+public class OrdersController(IOrderService orderService) : ControllerBase
 {
-    private readonly IOrderService _orderService;
-
-    public OrdersController(IOrderService orderService)
-    {
-        _orderService = orderService;
-    }
-
     [HttpPost]
-    public async Task<IActionResult> CreateOrder(OrderDto order)
+    public async Task<IActionResult> CreateOrder(OrderDownDto order)
     { 
-        await _orderService.AddAsync(order);
+        await orderService.AddAsync(order);
         return Created((Uri?)null, order);
     }
 
     [HttpGet("{id}")]
     public async Task<IActionResult> GetOrder(Guid id)
     {
-        var order = await _orderService.GetByIdAsync(id);
+        var order = await orderService.GetByIdAsync(id);
         if (order == null) return NotFound();
         return Ok(order);
+    }
+
+    [HttpGet("{userId}/by-user")]
+    public async Task<IActionResult> GetOrdersByUserId(string userId)
+    {
+        var orders = await orderService.GetByUserIdAsync(userId);
+        if (orders == null || orders.Length == 0) return NotFound();
+        return Ok(orders);
     }
 
     [HttpDelete]
     public async Task<IActionResult> DeleteOrder(Guid id)
     {
-        var order = await _orderService.GetByIdAsync(id);
-        if (order == null) return NotFound();
-        await _orderService.DeleteAsync(id);
+
+        await orderService.DeleteAsync(id);
         return NoContent();
     }
 }
